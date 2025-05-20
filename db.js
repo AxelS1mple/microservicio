3e03d4b72a08cb8bee01ff2560630091634a5244
Axel Nadir <axeltettw@gmail.com>
2025-05-19 23:54:14 -0600

    micro v3

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
 const mysql = require('mysql2');
+const fs = require('fs');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL_CA)
+  },
 });
 
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error de conexión:', err.message);
+    return;
+  }
   console.log('Conectado a MySQL');
 });
 
 module.exports = connection;
-    
--- routes/person.js
+++ routes/person.js
@@ -2,49 +2,35 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+
+
+
+
 // Insertar datos (POST /guardar)
 router.post('/guardar', (req, res) => {
   const {
-    nombre, apellido_paterno, apellido_materno,
-    curp, clave_elector, direccion,
-    municipio, estado, codigo_postal,
-    anio_registro, vigencia
+clave_lector, curp, nombres, apellido_paterno, apellido_materno, fecha_nacimiento, genero
   } = req.body;
 
   const sql = `
-    INSERT INTO personas (
-      nombre,
-      apellido_paterno,
-      apellido_materno,
-      curp,
-      clave_elector,
-      direccion,
-      municipio,
-      estado,
-      codigo_postal,
-      anio_registro,
-      vigencia
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+INSERT INTO personas (
+  clave_lector, curp, nombres, apellido_paterno, apellido_materno, fecha_nacimiento, genero
+) VALUES (?, ?, ?, ?, ?, ?, ?)
   `;
 
   db.query(sql, [
-    nombre,
-    apellido_paterno,
-    apellido_materno,
-    curp,
-    clave_elector,
-    direccion,
-    municipio,
-    estado,
-    codigo_postal,
-    anio_registro,
-    vigencia
+    clave_lector, curp, nombres, apellido_paterno, apellido_materno, fecha_nacimiento, genero
   ], (err, result) => {
     if (err) return res.status(500).send(err);
     res.status(201).send({ message: 'Datos guardados correctamente' });
   });
 });
+
+
+
+
+
+
 
 // Consultar todos los registros (GET /personas)
 router.get('/personas', (req, res) => {
@@ -55,5 +41,51 @@ router.get('/personas', (req, res) => {
     res.status(200).json(results);
   });
 });
+
+
+
+// Consultar todos los registros by id (GET /personas)
+router.get('/personasbyID/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM personas where id = ?`;
+
+  db.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).send(err);
+    if (results.length === 0) return res.status(404).send({ message: 'Not found'})
+    res.status(200).json(results);
+  });
+});
+
+
+
+
+
+// Consultar todos los registros by id (GET /personas)
+router.get('/personasbyCurp/:curp', (req, res) => {
+  const curp = req.params.curp;
+  const sql = `SELECT * FROM personas where curp = ?`;
+
+  db.query(sql, [curp], (err, results) => {
+    if (err) return res.status(500).send(err);
+    if (results.length === 0) return res.status(404).send({ message: 'Not found'})
+    res.status(200).json(results);
+  });
+});
+
+
+
+
+
+
+router.delete('/eliminarPersona/:id', (res, req) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM personas where id = ?`;
+
+  db.query(sql, [id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    if (result.affectedRows === 0) return res.status(404).json({ message: 'Persona no encontrada' });
+    res.status(200).json({ message: 'Delete confirmated'});
+  })
+})
 
 module.exports = router;
